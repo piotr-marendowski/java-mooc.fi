62532f1894cba171e7d844569d2ae0e5a2860534
Piotr Marendowski <piotr-marendowski@tutanota.com>
2023-10-24 15:47:55 +0000

    Add solution

---
+++ 14/Shanghai.java
@@ -0,0 +1,52 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class Shanghai extends Application {
+    @Override
+    public void start(Stage stage) {
+        // create the x and y axes that the chart is going to use
+        NumberAxis xAxis = new NumberAxis(2006, 2018, 4);
+        NumberAxis yAxis = new NumberAxis(0, 100, 10);
+
+        // set the titles for the axes
+        xAxis.setLabel("Year");
+        yAxis.setLabel("Ranking");
+
+        // create the line chart. The values of the chart are given as numbers
+        // and it uses the axes we created earlier
+        LineChart<Number, Number> lineChart = new LineChart<>(xAxis, yAxis);
+        lineChart.setTitle("University of Helsinki, Shanghai ranking");
+
+        // create the data set that is going to be added to the line chart
+        XYChart.Series ranking = new XYChart.Series();
+        ranking.setName("Ranking");
+        // and single points into the data set
+        ranking.getData().add(new XYChart.Data(2007, 73));
+        ranking.getData().add(new XYChart.Data(2008, 68));
+        ranking.getData().add(new XYChart.Data(2009, 72));
+        ranking.getData().add(new XYChart.Data(2010, 72));
+        ranking.getData().add(new XYChart.Data(2011, 74));
+        ranking.getData().add(new XYChart.Data(2012, 73));
+        ranking.getData().add(new XYChart.Data(2013, 76));
+        ranking.getData().add(new XYChart.Data(2014, 73));
+        ranking.getData().add(new XYChart.Data(2015, 67));
+        ranking.getData().add(new XYChart.Data(2016, 56));
+        ranking.getData().add(new XYChart.Data(2017, 56));
+
+        // add the data set to the line chart
+        lineChart.getData().add(ranking);
+
+        // display the line chart
+        Scene view = new Scene(lineChart, 640, 480);
+        stage.setScene(view);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(Shanghai.class);
+    }
+}
